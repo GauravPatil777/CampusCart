f71a02492176b9a4e1ca1d2f9daad90e15b49432
GauravPatil777 <gmp777pg@gmail.com>
2026-05-10 04:59:40 +0530

    render ready

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "true",
     credentials: true
 }));
 app.use(cookieParser());
