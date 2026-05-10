cc2bbedcc3920600dd7bdf2c08cf6a67786118ba
GauravPatil777 <gmp777pg@gmail.com>
2026-05-10 13:20:33 +0530

    cookie handle

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -272,7 +272,7 @@ export const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,        // true in production (HTTPS)
-            sameSite: none,      // helps with CSRF
+            sameSite: "None",      // helps with CSRF
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         })
         return res.status(200).json({
